c70b0259284af2d21264b35ccd9aee46c27c2ece
Arturianas <arturasdomnenko@gmail.com>
2023-04-13 11:55:54 +0300

    add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,19 @@ import Datepicker from 'react-tailwindcss-datepicker'
 
 import { formatDate } from '../utils/dateFuncs'
 
+type DateType = string | Date | number | null
+interface DateRange {
+  startDate: DateType
+  endDate: DateType
+}
+
 const Home = () => {
-  const [value, setValue] = useState({
+  const [value, setValue] = useState<DateRange | null>({
     startDate: new Date(),
     endDate: new Date().setMonth(11)
   })
 
-  const handleValueChange = (newValue) => {
+  const handleValueChange = (newValue: DateRange | null) => {
     console.log('newValue:', newValue)
     setValue(newValue)
   }
