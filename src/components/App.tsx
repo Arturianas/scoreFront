e148cade09623d99f2147f88f76fd19899cac243
Arturianas <arturasdomnenko@gmail.com>
2023-04-13 09:22:18 +0300

    scoreFront setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 const App = () => {
-  return <div className="flex justify-center bg-gray-300">Hello, world</div>
+  return (
+    <div className="flex justify-center bg-green-500">Lets scrape the web!</div>
+  )
 }
 
 export default App
