0c14b49f99267f24d7b36511d31ae858424bced9
Marius4568 <mariolee303@gmail.com>
2023-04-15 16:15:46 +0300

    now csv download function works

---
+++ src/utils/csvFuncs.ts
@@ -0,0 +1,14 @@
+export const downloadCSV = (csvData: string, filename: string) => {
+  const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' })
+
+  const link = document.createElement('a')
+  const url = URL.createObjectURL(blob)
+
+  link.setAttribute('href', url)
+  link.setAttribute('download', filename)
+  link.style.visibility = 'hidden'
+
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
